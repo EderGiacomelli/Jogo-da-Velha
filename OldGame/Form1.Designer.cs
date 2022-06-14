d4704248e3e3d0461cf8ce13f77c9a60d566bd49
Safelink <safetmplink@hotmail.com>
2022-06-14 11:44:17 -0300

    Alterado icone

--- OldGame/Form1.Designer.cs
+++ OldGame/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace OldGame
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -238,8 +239,9 @@ namespace OldGame
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Jogo da Velha";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
